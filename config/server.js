b0246948626a21ee80f4a1b3646c7bdc0ab46f45
DShamsheerKhan <dshamsheer.k@gmail.com>
2025-07-23 07:13:31 +0300

    test13

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
     host: env('HOST', '0.0.0.0'),
     port: env.int('PORT', 1337),
     app: {
-      keys: env.array('APP_KEYS', ['NNhb25UD4Edk58kz3GMp1w==', '3LiXRhK57QgAjVKr9lxwqw==', 'dF40MdSq+ajvPullbcLI2g==', 'Jw8QvB4xKUwC6Fk8OhSqIg==']),
+      keys: env.array('APP_KEYS', ['72d322c57487e8fb2ece7b2cb01a2f9ae28764a4814db7bab3f747210d49b1e6']),
     },
     webhooks: {
       populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
